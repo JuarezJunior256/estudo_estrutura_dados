ad58803a1e23123ed0f4a5447495a8cbfbcd25aa
juarezjunior256 <juarezjunior.live@gmail.comm>
2024-04-23 02:25:49 -0300

    estudo em andamento

---
+++ src/Listaligadas.js
@@ -0,0 +1,55 @@
+function LinkedList() {
+    let Node = function(element) {
+        this.element = element
+        this.next = null
+    }
+
+    let length = 0
+    let head = null 
+
+    // adiciona um elemento no final da lista
+    this.append = function(element) {
+
+    }
+
+    // adiciona um elemento em uma posição especifica
+    this.insert = function(position, element) {
+
+    }
+
+    // remove um elemento em uma posição especifica
+    this.removeAt = function(position) {
+
+    }
+
+    // remove o elemento element
+    this.remove = function(element) {
+
+    }
+
+    // retorna a posição do elemento
+    this.indexOf = function(element) {
+
+    }
+
+    // retorna se está vazia ou não a instancia 
+    this.isEmpty = function() {
+
+    }
+
+    // retorna o tamanho da instancia
+    this.size = function() {
+
+    }
+
+    // converte em string
+    this.toString = function() {
+
+    }
+
+    // mostra no console
+    this.print = function() {
+
+    }
+
+}
