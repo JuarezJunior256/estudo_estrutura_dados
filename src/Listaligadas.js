a1996acd76c5184304ef46ee8648ead631b3b43a
juarezjunior256 <juarezjunior.live@gmail.comm>
2024-05-09 01:53:45 -0300

    estudo em andamento

--- src/Listaligadas.js
+++ src/Listaligadas.js
@@ -9,7 +9,22 @@ function LinkedList() {
 
     // adiciona um elemento no final da lista
     this.append = function(element) {
+        let node = new Node(element),
+        current
+        
+        if (head === null) {
+            head = node
+        } else {
+            current = head
 
+            while(current.next) {
+                current = current.next
+            }
+
+            current.next = node
+        }
+
+        length++
     }
 
     // adiciona um elemento em uma posição especifica
@@ -19,7 +34,25 @@ function LinkedList() {
 
     // remove um elemento em uma posição especifica
     this.removeAt = function(position) {
-
+        if (position > - 1 && position < length) {
+            let current = head,
+            previous,
+            index = 0
+            
+            if (position === 0) {
+                head = current.next
+            } else  {
+                while(index++ < position) {
+                    previous = current
+                    current = current.next
+                }
+                previous.next = current.next
+            }
+            length--
+            return current.element
+        } else {
+            return null
+        }
     }
 
     // remove o elemento element
@@ -44,12 +77,29 @@ function LinkedList() {
 
     // converte em string
     this.toString = function() {
+        let current = head,
+        string = ''
 
+        while(current) {
+            string += current.element + ' '
+            current = current.next
+        }
+
+        return string
     }
 
     // mostra no console
     this.print = function() {
-
+        console.log(this.toString());
     }
 
 }
+
+let ll = new LinkedList()
+
+ll.append('João')
+ll.append('Jose')
+ll.append('Maria')
+ll.print();
+ll.removeAt(1);
+ll.print();
