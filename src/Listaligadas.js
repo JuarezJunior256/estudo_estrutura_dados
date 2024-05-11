c05d5596e68ebcaee77d1e8a0fb15538aafa16bb
juarezyaayoo <juarez@yaayoo.com.br>
2024-05-11 12:03:56 -0300

    estudo em andamento

--- src/Listaligadas.js
+++ src/Listaligadas.js
@@ -29,7 +29,28 @@ function LinkedList() {
 
     // adiciona um elemento em uma posição especifica
     this.insert = function(position, element) {
+        if (position >= 0 && position <= length) {
+            let node = new Node(element),
+            current = head,
+            previous, 
+            index = 0
 
+            if (position === 0) {
+                node.next - current
+                head = node
+            } else {
+                while(index++ < position) {
+                    previous = current
+                    current = current.next
+                }
+                node.next = current
+                previous.next = node
+            }
+            length++
+            return true
+        } else {
+            return false
+        }
     }
 
     // remove um elemento em uma posição especifica
@@ -101,5 +122,5 @@ ll.append('João')
 ll.append('Jose')
 ll.append('Maria')
 ll.print();
-ll.removeAt(1);
+ll.insert(0, 'Carlos');
 ll.print();
